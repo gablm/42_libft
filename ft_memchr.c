cd65e036c9a5ada60a8089dc601be78e96056eab
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-08-15 13:59:27 +0100

    save

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,29 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ztest.c                                            :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gfragoso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/14 21:06:45 by gfragoso          #+#    #+#             */
-/*   Updated: 2023/08/15 13:56:09 by gfragoso         ###   ########.fr       */
+/*   Created: 2023/08/15 13:51:54 by gfragoso          #+#    #+#             */
+/*   Updated: 2023/08/15 13:54:45 by gfragoso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-int	main(int argc, char **argv)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	(void)argc;
-	(void)argv;
-	char a[] = "cringe";
-	//char b[] = "0000000000";
-	char *c = ft_memchr(a, 'i', 6);
-	ft_putstr(a);
-	ft_putchar(10);
-	ft_putstr(c);
-	
-	ft_putchar(10);
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (*((unsigned char *)s + i) == (unsigned char)c)
+			return (s + i);
+		i++;
+	}
 	return (0);
 }
