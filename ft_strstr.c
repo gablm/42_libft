f349c2c6809d580042a64ce33a6f2a4bab786605
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-08-15 14:44:05 +0100

    strchr

--- ft_strstr.c
+++ ft_strstr.c
@@ -12,27 +12,5 @@
 
 char	*ft_strstr(const char *big, char *little)
 {
-	int	i;
-	int	h;
-
-	i = 0;
-	h = 0;
-	if (little[0] == '\0')
-		return (big);
-	while (big[i])
-	{
-		if (big[i] == little[h])
-		{
-			h++;
-			if (little[h] == '\0')
-			{
-				h = i - h + 1;
-				return (big + h);
-			}
-		}
-		else
-			h = 0;
-		i++;
-	}
 	return (0);
-}
+} //redo
